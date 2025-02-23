a8bd996b4f875c4dfe6a00d3d46d1e6494e43b79
Vibhu Bindal <vibhubindal2013@gmail.com>
2025-02-23 13:11:02 +0530

    Update streamlit_app.py
    account details

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,14 @@ name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
 #session = get_active_session()
-cnx = st.connection("snowflake")
+cnx = st.connection("snowflake", type="snowflake", 
+                     account="BQBPFXK", 
+                     user="SUB32735",
+                     password="Vibhuwindows10@",
+                     warehouse="COMPUTE_WH",
+                     database="SMOOTHIES",
+                     schema="PUBLIC")
+
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
